cd1dccd399297a0c0956652d1773d5bacc410291
vijaym2211 <vijaym2211@gmail.com>
2024-08-07 18:15:21 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//package org.example.oop1;
+public class Main {
+    public static void main(String[] args) {
+        Student obj = new Student();
+        obj.marks=10;
+        obj.name="Prasad";
+        obj.age=20;
+        obj.sid=1;
+        obj.setSid();
+        obj.setage();
+        obj.setMarks();
+        obj.setName();
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,21 @@
+//package org.example.oop1;
+public class Student {
+    int sid;
+    int age;
+    float marks;
+    String name;
+
+    void setage(){
+        System.out.println("age:" +age);
+    }
+    void setMarks(){
+        System.out.println("marks:" +marks);
+    }
+    void setName(){
+        System.out.println("name:"+name);
+    }
+
+    void setSid() {
+        System.out.println("sid:"+sid);
+    }
+}
